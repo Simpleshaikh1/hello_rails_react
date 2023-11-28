cf7143e5066e0ec1f5cf38d2ea9c7ac47a0f6940
Toyyib <hamzatbolaji648@yahoo.com>
2023-11-28 14:57:27 +0100

    error corrected

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::GreetingsController < ApplicationController
   def random_greeting
     @greeting = Message.order('RANDOM()').first
-    render json: { greeting: @greeting.greeting }
+    render json: { greeting: @greeting.content }
   end
 end
